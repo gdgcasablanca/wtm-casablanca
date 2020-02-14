d5690ac81c6c7e836811c30fa5154ad3206d9b95
Omar Houmz <omhoumz@gmail.com>
2020-02-14 19:53:08 +0100

    chore: prettier

--- service-worker.js
+++ service-worker.js
@@ -14,8 +14,8 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v4.31';
-const RUNTIME = 'runtime';
+const PRECACHE = 'precache-v4.31'
+const RUNTIME = 'runtime'
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
@@ -27,31 +27,40 @@ const PRECACHE_URLS = [
   'favicon.png',
   'assets/images/logo-gdg.svg',
   'assets/images/logo-wtm.svg',
-  'assets/images/icons/icon-192x192.png'
-];
+  'assets/images/icons/icon-192x192.png',
+]
 
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(PRECACHE)
+    caches
+      .open(PRECACHE)
       .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
-  );
-});
+      .then(self.skipWaiting()),
+  )
+})
 
 // The activate handler takes care of cleaning up old caches.
 self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
+  const currentCaches = [PRECACHE, RUNTIME]
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
-  );
-});
+    caches
+      .keys()
+      .then(cacheNames => {
+        return cacheNames.filter(
+          cacheName => !currentCaches.includes(cacheName),
+        )
+      })
+      .then(cachesToDelete => {
+        return Promise.all(
+          cachesToDelete.map(cacheToDelete => {
+            return caches.delete(cacheToDelete)
+          }),
+        )
+      })
+      .then(() => self.clients.claim()),
+  )
+})
 
 // The fetch handler serves responses for same-origin resources from a cache.
 // If no response is found, it populates the runtime cache with the response
@@ -62,18 +71,18 @@ self.addEventListener('fetch', event => {
     event.respondWith(
       caches.match(event.request).then(cachedResponse => {
         if (cachedResponse) {
-          return cachedResponse;
+          return cachedResponse
         }
 
         return caches.open(RUNTIME).then(cache => {
           return fetch(event.request).then(response => {
             // Put a copy of the response in the runtime cache.
             return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
+              return response
+            })
+          })
+        })
+      }),
+    )
   }
-});
+})
