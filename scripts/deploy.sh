80be9fac2e806c52cbd8b1568e1bd8ee9fb53768
Omar Houmz <omarhoumz@gmail.com>
2020-09-11 00:20:16 +0100

    chore: scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,14 @@
 yarn
 yarn build
 
-if [ "$TRAVIS_BRANCH" = "develop" ]; then
-  firebase use develop
-elif [ "$TRAVIS_BRANCH" = "master" ]; then
-  firebase use default
-fi
+# if [ "$TRAVIS_BRANCH" = "develop" ]; then
+#   firebase use develop
+# elif [ "$TRAVIS_BRANCH" = "master" ]; then
+#   firebase use default
+# fi
+
+echo 'We are here'
+
+firebase projects:list --token $FIREBASE_TOKEN
 
 firebase deploy --token $FIREBASE_TOKEN
